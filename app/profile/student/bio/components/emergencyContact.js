6d5d41e6e1fe77fc1855aa4cff1750bb4ffc30aa
unknown <gsanjay@gsanjay-pc.pkrmsezdc.com>
2017-01-31 13:47:28 +0530

    Resolved issue "MYC-268"

--- app/profile/student/bio/components/emergencyContact.js
+++ app/profile/student/bio/components/emergencyContact.js
@@ -9,8 +9,8 @@ import BasicRow from '../../../../common/basicRow';
 const EmergencyContact = (props) => (
       <article className="profileRow mt30">
             <h3 className="dataHeading openSansRegular">Emergency Contact</h3>
-            <BasicRow displayName="First" displayValue={props.json.name.first} />
-            <BasicRow displayName="Last" displayValue={props.json.name.last} />
+            <BasicRow displayName="First name" displayValue={props.json.name.first} />
+            <BasicRow displayName="Last name" displayValue={props.json.name.last} />
             <div className="emailAddress"><BasicRow displayName="Phone" displayValue={props.json.phone} /></div>
             <BasicRow displayName="Relationship" displayValue={props.json.relation} />
       </article>
