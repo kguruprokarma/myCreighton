a989ea162a2c15720688269b293ec87cb916eae8
unknown <gsanjay@gsanjay-pc.pkrmsezdc.com>
2017-01-31 16:47:08 +0530

    changes in emergencyContact of profile

--- app/Profile/components/EmergencyContact.js
+++ app/Profile/components/EmergencyContact.js
@@ -5,8 +5,8 @@ import BasicRow from '../../components/BasicRow';
 const EmergencyContact =(props) => (
        <div className="profileRow mt30">
         <h3 className="dataHeading openSansRegular">Emergency Contact</h3>
-            <BasicRow displayName="First:" displayValue={props.json.name.first} />
-            <BasicRow displayName="Last:" displayValue={props.json.name.last} />
+            <BasicRow displayName="First name:" displayValue={props.json.name.first} />
+            <BasicRow displayName="Last name:" displayValue={props.json.name.last} />
             <BasicRow displayName="Phone:" displayValue={props.json.phone} />
             <BasicRow displayName="Relationship:" displayValue={props.json.relation} />
       </div>
