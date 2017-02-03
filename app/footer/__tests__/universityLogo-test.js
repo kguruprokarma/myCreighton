ab685d79e7f2b28089b1f8e1f4c6d13bd9614cdf
Ansar Shaik <sansar@pkrmsezdc.com>
2017-02-03 13:04:18 +0530

    test cases for footer component

---
+++ app/footer/__tests__/universityLogo-test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import UniversityLogo from '../components/universityLogo';
+
+describe('UniversityLogo component testing ----->', () => {
+	const UniversityLogoC = shallow(<UniversityLogo />);
+	it('UniversityLogo is defined', () => {
+		expect(UniversityLogoC).toBeDefined();
+	});
+
+	it('UniversityLogo component should contain image tag', () => {
+		expect(UniversityLogoC.find('img').length).toBe(1);
+	});
+});
