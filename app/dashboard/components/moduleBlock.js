661ffe22a078cbb7e1f2e717b072c4d2f3087e0e
unknown <gsanjay@gsanjay-pc.pkrmsezdc.com>
2017-02-07 12:46:05 +0530

    Classes Link

--- app/dashboard/components/moduleBlock.js
+++ app/dashboard/components/moduleBlock.js
@@ -10,17 +10,19 @@ const ModuleBlock = (modulesprops) => {
 
 	const moduleBlocks = modulesprops.modulelist.map((moduleDetail, moduleIndex) => {
 		return (
+			<Link to={moduleDetail.linkto}>
 			<Well bsSize="large" className="white-well" key={moduleIndex}>
 				<Row>
 					<Col xs={3} md={12}  className="moduleImage">
 						<img src={moduleDetail.imgURL} />
 					</Col>
 					<Col xs={9} md={12}>
-						<h3 className="well-title openSansLight"><Link to={moduleDetail.linkto}>{moduleDetail.name}</Link> {moduleDetail.name==='School & Semester'? <span className="well-badge"><span>1</span></span> : '' }</h3>
+						<h3 className="well-title openSansLight">{moduleDetail.name}{moduleDetail.name==='School & Semester'? <span className="well-badge"><span>1</span></span> : '' }</h3>
 						<div className="well-caption">{moduleDetail.description}</div>
 					</Col>
 				</Row>
 			</Well>
+			</Link> 
 		);
 	})
 	return (
