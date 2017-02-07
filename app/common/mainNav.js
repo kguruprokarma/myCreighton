81f04a8961f108d15cc4f110469ac2159cda0451
selladurai14 <aselladurai@pkrmsezdc.com>
2017-02-07 19:48:04 +0530

    MYC-465: Creighton Services and Resources link is not displaying under Hamburger Menu

--- app/common/mainNav.js
+++ app/common/mainNav.js
@@ -9,10 +9,11 @@ class MainNav extends React.Component {
     render() {
         return (
             <ul className="list-group menu-xs gbl_full-width">
+                <li className="list-group-item"> <a href="#">Creighton Services and Resources</a></li>
                 <li className="list-group-item"> <a href="#">Library Search</a></li>
+                <li className="list-group-item"><a href="#">DoIT Services & Support</a></li>             
                 <li className="list-group-item"><a href="#">Where To Find It</a></li>
-                <li className="list-group-item"><a href="#">Campus Directory</a></li>
-                <li className="list-group-item"><a href="#">DoIT Services & Support</a></li>
+                <li className="list-group-item"><a href="#">Campus Directory</a></li>                
                 <li className="list-group-item"><a href="#">Academic Calendar</a></li>
                 <li className="list-group-item"><a href="#">Campus Map</a></li>
                 <li className="list-group-item"><a href="#" >Notifications</a><span className="well-badge"><span>1</span></span></li>
