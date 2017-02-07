f417c100870ce0c6cfd3764b7f4eae4b9fb7da69
selladurai14 <aselladurai@pkrmsezdc.com>
2017-02-07 18:31:05 +0530

    Fixed: MYC-464: Hamburger Menu is not displaying properly

--- app/common/mainNav.js
+++ app/common/mainNav.js
@@ -8,14 +8,14 @@ import { Link } from 'react-router';
 class MainNav extends React.Component {
     render() {
         return (
-            <ul className="list-group">
+            <ul className="list-group menu-xs gbl_full-width">
                 <li className="list-group-item"> <a href="#">Library Search</a></li>
                 <li className="list-group-item"><a href="#">Where To Find It</a></li>
                 <li className="list-group-item"><a href="#">Campus Directory</a></li>
                 <li className="list-group-item"><a href="#">DoIT Services & Support</a></li>
                 <li className="list-group-item"><a href="#">Academic Calendar</a></li>
                 <li className="list-group-item"><a href="#">Campus Map</a></li>
-                <li className="list-group-item"><a href="#" >Notifications</a></li>
+                <li className="list-group-item"><a href="#" >Notifications</a><span className="well-badge"><span>1</span></span></li>
             </ul>
         );
     }
