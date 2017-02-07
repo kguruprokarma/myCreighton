642c853751024fd1bc373179a61c866cfbe25672
PKRMSEZDC\pkotapally <pkotapally@prokarma.com>
2017-02-07 18:32:48 +0530

    MYC-141, MYC-147, MYC-448 and MYC-347 # : hamburger menu, classes list controllers styling for mobile

--- app/common/mainNav.js
+++ app/common/mainNav.js
@@ -4,19 +4,29 @@
 
 import React from 'react';
 import { Link } from 'react-router';
+import { Grid, Row } from 'react-bootstrap';
 
 class MainNav extends React.Component {
     render() {
         return (
-            <ul className="list-group">
-                <li className="list-group-item"> <a href="#">Library Search</a></li>
-                <li className="list-group-item"><a href="#">Where To Find It</a></li>
-                <li className="list-group-item"><a href="#">Campus Directory</a></li>
-                <li className="list-group-item"><a href="#">DoIT Services & Support</a></li>
-                <li className="list-group-item"><a href="#">Academic Calendar</a></li>
-                <li className="list-group-item"><a href="#">Campus Map</a></li>
-                <li className="list-group-item"><a href="#" >Notifications</a></li>
-            </ul>
+
+            <nav role="Navigation" id="navigation" className={this.props.navDisplay ? 'openNav' : 'closeNav'}>
+                <h1 className="announced-only">Navigation</h1>
+                <Grid>
+                    <Row>
+                        <ul className="main-navigation">
+                            <li className="navigation-item"><a href="#">Library Search <span className="glyphicon glyphicon-chevron-right pull-right hidden-lg hidden-md hidden-sm"></span></a></li>
+                            <li className="navigation-item"><a href="#">Where To Find It <span className="glyphicon glyphicon-chevron-right pull-right hidden-lg hidden-md hidden-sm"></span></a></li>
+                            <li className="navigation-item"><a href="#">Campus Directory <span className="glyphicon glyphicon-chevron-right pull-right hidden-lg hidden-md hidden-sm"></span></a></li>
+                            <li className="navigation-item"><a href="#">DoIT Services & Support <span className="glyphicon glyphicon-chevron-right pull-right hidden-lg hidden-md hidden-sm"></span></a></li>
+                            <li className="navigation-item"><a href="#">Academic Calendar <span className="glyphicon glyphicon-chevron-right pull-right hidden-lg hidden-md hidden-sm"></span></a></li>
+                            <li className="navigation-item"><a href="#">Campus Map <span className="glyphicon glyphicon-chevron-right pull-right hidden-lg hidden-md hidden-sm"></span></a></li>
+                            <li className="navigation-item"><a href="#" >Notifications <span className="glyphicon glyphicon-chevron-right pull-right hidden-lg hidden-md hidden-sm"></span></a></li>
+                        </ul>
+                    </Row>
+                </Grid>
+            </nav>
+
         );
     }
 }
--- app/header/index.js
+++ app/header/index.js
@@ -18,20 +18,20 @@ class Header extends React.PureComponent {
     constructor(props) {
         super(props);
         this.state = {
-            showNav:false
+            showNav: false
         }
         this.showPopUp = this.showPopUp.bind(this);
         this.goBack = this.goBack.bind(this);
         this.navClick = this.navClick.bind(this);
     }
     showPopUp() {
-        if(!this.props.popUpData){
+        if (!this.props.popUpData) {
             this.props.popUpOpen();
-        }else{
+        } else {
             this.props.popUpClose();
         }
     }
-    navClick(){
+    navClick() {
         this.setState({ showNav: !this.state.showNav });
     }
     goBack() {
@@ -46,47 +46,48 @@ class Header extends React.PureComponent {
 
         return (
             <div>
-            <header>
-                <h1 className="announced-only">Page header</h1>
-                <div className="container">
-                    <Row >
-                        <Col xs={2} md={6} className="visible-xs hamburgerMenu">
-                            <img src={'./assets/images/menu.png'} onClick={this.navClick} />
-                        </Col>
-
-                        <Col xs={8} md={6}>
-                            <Title path={this.props.currentState} classDetails={title}/>
-                        </Col>
-                        <Col xs={2} md={6}>
-                            <ul className="pull-right list-inline">
-                                <li className="head-Icons">
-                                    <div className='popUp'>
-                                        <span className='glyphicon glyphicon-user' onClick={this.showPopUp}></span>
-                                        <div className='popUpContainer'>
-                                        {this.props.popUpData &&
-                                                <ProfileMenu showPopValue={this.showPopUp} />}
+                <header>
+                    <h1 className="announced-only">Page header</h1>
+                    <div className="container">
+                        <Row >
+                            <Col xs={2} sm={2} md={6} className="visible-xs hamburgerMenu">
+                                <img src={'./assets/images/menu.png'} onClick={this.navClick} />
+                            </Col>
+                            <Col xs={8} md={6} sm={10} className="hidden-xs">
+                                <h2 className="bebasregular logo mt10 mb10 fs1pt4">MYCREIGHTON</h2>
+                            </Col>
+                            <Col xs={8} md={6} sm={8} className="visible-xs">
+                                <Title path={this.props.currentState} classDetails={title} />
+                            </Col>
+                            <Col xs={2} md={6} sm={2}>
+                                <ul className="pull-right list-inline">
+                                    <li className="head-Icons">
+                                        <div className='popUp'>
+                                            <span className='glyphicon glyphicon-user' onClick={this.showPopUp}></span>
+                                            <div className='popUpContainer'>
+                                                {this.props.popUpData &&
+                                                    <ProfileMenu showPopValue={this.showPopUp} />}
+                                            </div>
                                         </div>
-                                    </div>
-                                </li>
-                            </ul>
-                        </Col>
-                    </Row>
-                </div>
-            </header>
-            {this.state.showNav && <Navigation />}
+                                    </li>
+                                </ul>
+                            </Col>
+                        </Row>
+                    </div>
+                </header>
+                {/* Main Navigation */}
+                <Navigation navDisplay={this.state.showNav} />
+                {/* ./Main Navigation */}
             </div>
         );
     }
 }
 
-
-
-
 const mapStateToProps = (storeData) => (
-{
-    classDetails: storeData.classDetailsReducer.classDetails,
-    popUpData: storeData.headerReducer.showPopUp
-})
+    {
+        classDetails: storeData.classDetailsReducer.classDetails,
+        popUpData: storeData.headerReducer.showPopUp
+    })
 
 const mapDispatchToProps = (dispatch) => bindActionCreators(Object.assign(actionCreators), dispatch)
 
