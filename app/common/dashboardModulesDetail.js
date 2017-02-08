282395ddacc5cdbef31d6edb678bf740bc980745
AliUsmaan <musmanali@prokarma.com>
2017-02-08 14:45:30 +0530

    Issue MYC-467

--- app/common/dashboardModulesDetail.js
+++ app/common/dashboardModulesDetail.js
@@ -14,25 +14,20 @@ const modulesData = [
 			"description": "School Stuff I am responsible for."
 		},
 		{
-			"imgURL": "./assets/images/classes.png",
-			"linkto": "/classes",
-			"name": "Classes",
-			"description": "What I  need for each className."
-		}
-	],
-	[
-		{
-			"imgURL": "./assets/images/todo.png",
-			"linkto": "",
-			"name": "To-Do List",
-			"description": "What I need to do."
-		},
-		{
 			"imgURL": "./assets/images/nextevent.png",
 			"linkto": "",
 			"name": "Next Events",
 			"description": "What I need to be ready for."
 		}
+		
+	],
+	[
+		{
+			"imgURL": "./assets/images/classes.png",
+			"linkto": "/classes",
+			"name": "Classes",
+			"description": "What I  need for each className."
+		}		
 
 	]
 ];
