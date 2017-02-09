b2423f0f71d84dbbf0262c2dfb2a89055ac8e683
Ansar Shaik <sansar@pkrmsezdc.com>
2017-02-09 18:15:11 +0530

    added the test case i18n mocking

--- app/dashboard/mealPlan/__tests__/mealPlanView-test.js
+++ app/dashboard/mealPlan/__tests__/mealPlanView-test.js
@@ -4,32 +4,42 @@
 import React from 'react';
 import { shallow, mount, render  } from 'enzyme';
 import {MealPlan} from '../index';
-
+import i18n from '../../../i18n';
 const defaultProps ={
     getMealPlanData:()=>{},
     mealPlanData:{
-    "mealBalance": {
-        "remainSwipes": "13",
-        "swipesExpire": "SAT",
-        "guestSwipes": "5",
-        "dinning": "360.00",
-        "jaybucks": "750.25"
+        "mealBalance": {
+            "remainSwipes": "13",
+            "swipesExpire": "SAT",
+            "guestSwipes": "5",
+            "dinning": "360.00",
+            "jaybucks": "750.25"
+        }
+    },
+    common:{
+        "MEAL_PLAN":"Food-123",
+        "SWIPES":"Gold",
+        "Guest":"Silver",
+        "Dining":"Steel",
+        "Jaybucks":"Consumption"
     }
 }
-}
 
-const mealPlanC = shallow(
-        <MealPlan  {...defaultProps}/>
-  );
+i18n.t = jest.fn((val) => {
+    return defaultProps.common[val.split(':')[1]];
+});
+
+const mealPlanC = shallow(<MealPlan {...defaultProps}/>);
 describe('I18nextProvidergg', () => {
 
     it('should provide i18n context', () => {
         expect(mealPlanC).toBeDefined();
     });
-    
+
     it('MealPlan component Swipes is there or not', () => {
         expect(mealPlanC.find('Swipes').length).toBe(1);
         expect(mealPlanC.find('Swipes').props().swipeCount).toBe(defaultProps.mealPlanData.mealBalance.remainSwipes);
+        expect(mealPlanC.find('Swipes').props().swipeTitle).toBe(defaultProps.common.SWIPES);
     });
 
     it('MealPlan component Guest is there or not', () => {
