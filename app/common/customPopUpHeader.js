2c0d834e01df04d5b4ed02b0a14effa3c4825e96
selladurai14 <aselladurai@pkrmsezdc.com>
2017-02-09 23:58:06 +0530

    MYC-432: Hamburger Menu - Staff/Faculty - Mobile

--- app/common/customPopUpHeader.js
+++ app/common/customPopUpHeader.js
@@ -6,7 +6,7 @@ class CustomPopUpHeader extends React.Component {
   render() {
     return (<div className='customPopUpHeader'>
       <Avatar name={this.props.headerData.userName.fullName} size={25} round={true} />
-      <span className='tileInfo' >{this.props.headerData.userName.fullName}</span>
+      <span className='tileInfo' >{this.props.headerData.userName.fullName}</span><span className="openSansBold user-type"> - {this.props.headerData.userRole}</span>
     </div>
     );
   }
