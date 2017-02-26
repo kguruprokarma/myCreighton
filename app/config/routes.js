271f3bafeb43656f6c59644b324d63e6e4c11f87
selladurai14 <aselladurai@pkrmsezdc.com>
2017-02-26 23:45:10 +0530

    merge changes for class details

--- app/config/routes.js
+++ app/config/routes.js
@@ -15,16 +15,13 @@ import ClassDetails from '../classes/classDetails';
 import Dashboard from '../dashboard/index';
 import StaffProfile from '../profile/staff';
 import FacultyProfile from '../profile/faculty';
+
 import { I18nextProvider } from 'react-i18next'; // as we build ourself via webpack
 import i18n from '../i18n'; // initialized i18next instance
 import * as ROUTE_URL from '../constants/routeContants';
 import EventList from '../nextEvents/eventList';
-<<<<<<< HEAD
 import EventDetails from '../nextEvents/eventDetails';
-=======
-import EventsDetail from '../nextEvents/eventsDetail';
 import * as interceptor from './interceptor';
->>>>>>> 727957dd58afb88d08f7fc58c3685b94f3290ff0
 
 const store = configureStore();
 const history = syncHistoryWithStore(hashHistory, store);
@@ -44,7 +41,7 @@ const routes = (
         <Route path={ROUTE_URL.STAFF }  title="Staff Profile -" component={StaffProfile} />
         <Route path={ROUTE_URL.EVENT_LIST} title='' component={EventList} />
         <Route path={ROUTE_URL.FACULTY }  title="Faculty Profile -" component={FacultyProfile} />
-        <Route path={ROUTE_URL.CLASS_EVENT }  title="Staff Profile -" component={ClassEvent} />
+        
         <Route path={ROUTE_URL.EVENT_DETAILS + ROUTE_URL.EVENT_DETAILS_TYPE + ROUTE_URL.ID_PARAM} title='Event Detail - ' component={EventDetails} />
       </Route>
     </Router>
