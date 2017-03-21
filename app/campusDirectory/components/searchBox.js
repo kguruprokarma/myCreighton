074fdf60ee210d14e68d4ac2a8ac5957015b0dc3
PKRMSEZDC\pkotapally <pkotapally@prokarma.com>
2017-03-21 18:08:14 +0530

    Styling of campus directory simple search

--- app/campusDirectory/components/searchBox.js
+++ app/campusDirectory/components/searchBox.js
@@ -13,13 +13,21 @@ const SearchBox = (props) => (
     <Row>
       <Col xs={12}>
         <Form>
-          <FormGroup>
-            <FormControl type='search' placeholder={translateText('common:SEARCH_CREIGHTON_STAFF')} />
-            <HelpBlock>{translateText('common:EXAMPLE_CAMPUS_DIRECTORY_SEARCH')} </HelpBlock>
-          </FormGroup>
-          <FormGroup>
-            <Button className='form-control'>{translateText('common:SEARCH')}</Button>
-          </FormGroup>
+          <Row>
+            <Col sm={3}>
+            </Col>
+            <Col xs={12} sm={6}>
+              <FormGroup>
+                <FormControl type='search' className='openSansLight cmpsDirSearch mt20' placeholder={translateText('common:SEARCH_CREIGHTON_STAFF')} />
+                <HelpBlock className='openSansRegular cmpsDirText mt10'>{translateText('common:EXAMPLE_CAMPUS_DIRECTORY_SEARCH')} </HelpBlock>
+              </FormGroup>
+            </Col>
+            <Col xs={12} sm={3}>
+              <FormGroup>
+                <Button className='form-control openSansLight cmpsDirSearchBtn mt20 pl10'>{translateText('common:SEARCH')}</Button>
+              </FormGroup>
+            </Col>
+          </Row>
         </Form>
       </Col>
     </Row>
