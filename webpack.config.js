996df8167a5ce7a85d38d2c1ad2f1833ec536165
Kishore Kumar Guru <kguru@pkrmsezdc.com>
2017-03-22 18:26:42 +0530

    For Testing Jenkins

--- webpack.config.js
+++ webpack.config.js
@@ -13,8 +13,8 @@ Object.assign(exports, {
   },
   output: {
     path: path.join(__dirname, 'build'),
-    filename: '[name].[BUILD_NUMBER].js',
-    chunkFilename: '[name].[BUILD_NUMBER].js',
+    filename: '[name].[id].js',
+    chunkFilename: '[name].[id].js',
     publicPath: '/'
   },
   module: {
@@ -79,7 +79,7 @@ Object.assign(exports, {
     }),
     new webpack.optimize.CommonsChunkPlugin({
       name: 'vendor',
-      filename: '[name].[BUILD_NUMBER].js'
+      filename: '[name].[id].js'
     })
   ]
 });
