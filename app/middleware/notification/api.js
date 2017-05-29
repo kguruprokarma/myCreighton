73eb0abaac454204cca67a0971db6b805fe44575
PKRMSEZDC\kguru <kguru@prokarma.com>
2017-05-29 16:38:53 +0530

    app/middleware/notification/api.js File updated to hit Prokarma local Dev

--- app/middleware/notification/api.js
+++ app/middleware/notification/api.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 import * as urlConstants from '../../constants/urlConstants';
 
 const NotificationApi = {
-  getNotifications: () => axios.get('http://localhost:8081/notifications'),
-  sendNotification: (message) => axios.post('http://localhost:8081/newnotifications', {newNotification: message})
+  getNotifications: () => axios.get('http://172.16.100.45:8081/notifications'),
+  sendNotification: (message) => axios.post('http://172.16.100.45:8081/newnotifications', {newNotification: message})
 };
 
 export default NotificationApi;
